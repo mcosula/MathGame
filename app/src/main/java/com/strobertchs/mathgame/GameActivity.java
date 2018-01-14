71da0990485ef1292839979da17cb506354f3588
mcsoula <PerseusJackson600@gmail.com>
2018-01-14 14:24:22 -0500

    Game Screen

--- app/src/main/java/com/strobertchs/mathgame/GameActivity.java
+++ app/src/main/java/com/strobertchs/mathgame/GameActivity.java
@@ -2,10 +2,16 @@ package com.strobertchs.mathgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+    int correctAnswer;
+    Button buttonObjectChoice1;
+    Button buttonObjectChoice2;
+    Button buttonObjectChoice3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +19,7 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         int partA = 9;
         int partB = 9;
-        int correctAnswer = partA * partB;
+        correctAnswer = partA * partB;
         int wrongAnswer1 = correctAnswer - 1;
         int wrongAnswer2 = correctAnswer + 1;
         TextView textObjectPartA =
@@ -22,13 +28,13 @@ public class GameActivity extends AppCompatActivity {
         TextView textObjectPartB =
                 (TextView)findViewById(R.id.textPartB);
 
-        Button buttonObjectChoice1 =
+        buttonObjectChoice1 =
                 (Button)findViewById(R.id.buttonChoice1);
 
-        Button buttonObjectChoice2 =
+        buttonObjectChoice2 =
                 (Button)findViewById(R.id.buttonChoice2);
 
-        Button buttonObjectChoice3 =
+        buttonObjectChoice3 =
                 (Button)findViewById(R.id.buttonChoice3);
 
         textObjectPartA.setText("" + partA);
@@ -36,5 +42,61 @@ public class GameActivity extends AppCompatActivity {
         buttonObjectChoice1.setText("" + correctAnswer);
         buttonObjectChoice2.setText("" + wrongAnswer1);
         buttonObjectChoice3.setText("" + wrongAnswer2);
+
+        buttonObjectChoice1.setOnClickListener(this);
+        buttonObjectChoice2.setOnClickListener(this);
+        buttonObjectChoice3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int answerGiven = 0;
+        switch (view.getId()){
+            case R.id.buttonChoice1:
+            // button 1
+                answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
+
+                if(answerGiven==correctAnswer) {
+                    Toast.makeText(getApplicationContext(),
+                            "Well done!",
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry that's wrong",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            case R.id.buttonChoice2:
+            // button 2
+                answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
+
+                if(answerGiven==correctAnswer) {
+                    Toast.makeText(getApplicationContext(),
+                            "Well done!",
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry that's wrong",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            case R.id.buttonChoice3:
+            // button 3
+                answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
+
+                if(answerGiven==correctAnswer) {
+                    Toast.makeText(getApplicationContext(),
+                            "Well done!",
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry that's wrong",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+
+        }
     }
 }
